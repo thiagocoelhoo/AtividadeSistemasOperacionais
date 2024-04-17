2c2a46edd99695d6411ca6bfd397395f1d60c93d
Thiago Coelho <thiago.coelho158@gmail.com>
2024-04-17 11:43:36 -0300

    fix: consertar bug em valor de pc syscall Fork

--- mars/mips/instructions/syscalls/SyscallFork.java
+++ mars/mips/instructions/syscalls/SyscallFork.java
@@ -3,6 +3,7 @@ package mars.mips.instructions.syscalls;
 import mars.ProgramStatement;
 import mars.mips.SO.ProcessManager.ProcessControlBlock;
 import mars.mips.SO.ProcessManager.ProcessTable;
+import mars.mips.hardware.RegisterFile;
 
 public class SyscallFork extends AbstractSyscall {
     public SyscallFork() {
@@ -13,7 +14,10 @@ public class SyscallFork extends AbstractSyscall {
      * Cria um novo processo na ProcessTable
      */
     public void simulate(ProgramStatement statement) {
-        int pc = statement.getAddress();
-        ProcessTable.createProcess(pc);
+        // int pc = statement.getAddress();
+        int regNumber = RegisterFile.getNumber("$a0");
+        int pc = RegisterFile.getValue(regNumber);
+        ProcessControlBlock process = ProcessTable.createProcess(pc);
+        System.out.printf("Syscall fork: $a0 = %d; PID = %d\n", pc, process.getPID());
     }
 }
